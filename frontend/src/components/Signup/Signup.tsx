b86e62ec91603322bd7d2be8d880dab9ecbe749c
jdubilla <jbdubillard@hotmail.com>
2023-07-08 21:01:34 +0200

    Add Signup component

--- frontend/src/components/Signup/Signup.tsx
+++ frontend/src/components/Signup/Signup.tsx
@@ -1,7 +1,17 @@
+import logo from '../../assets/logo.png'
 import s from './style.module.css'
 
 export function Signup() {
 	return (
-		<div>Test</div>
+		<div className={s.container}>
+			<div className={s.bgSignupBox}>
+				<div className={s.signupBox}>
+					<div className={s.name}>
+						<img className={s.image} src={logo} alt='logo' />
+						Join Flirtopia now
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -40,7 +40,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+// import { App } from './App';
 import { Signup } from './components/Signup/Signup'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
