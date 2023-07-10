d579b7a04f53ced926edd8e5211af4f2eaf92093
jdubilla <jbdubillard@hotmail.com>
2023-07-11 00:30:10 +0200

    Clean code

--- frontend/src/pages/Age/Age.tsx
+++ frontend/src/pages/Age/Age.tsx
@@ -1,18 +1,39 @@
-import { FormEvent } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { ButtonNext } from '../../components/ButtonNext/ButtonNext';
 import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/calendar.png'
-import s from './style.module.css'
+import logo from '../../assets/calendar.png';
+import s from './style.module.css';
 
 export function Age() {
-
 	const navigate = useNavigate();
+	const [selectedDay, setSelectedDay] = useState('');
+	const [selectedMonth, setSelectedMonth] = useState('');
+	const [selectedYear, setSelectedYear] = useState('');
+	const [legalAge, setLegalAge] = useState(true);
+
+	const days = Array.from(Array(31), (_, index) => index + 1);
+	const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+					'August', 'September', 'October', 'November', 'December'];
+	const currentYear = new Date().getFullYear();
+	const years = Array.from(Array(100), (_, index) => currentYear - index);
+
+	useEffect(() => {
+			if (selectedYear !== '' && selectedMonth !== '' && selectedDay !== '') {
+				const selectedDate = new Date(
+					parseInt(selectedYear),
+					parseInt(selectedMonth) - 1,
+					parseInt(selectedDay)
+					);
+				const eighteenYearsAgo = new Date();
+				eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+				setLegalAge(selectedDate <= eighteenYearsAgo);
+			} else {
+				setLegalAge(true);
+			}
+	}, [selectedDay, selectedMonth, selectedYear]);
 
 	function handleSubmit(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const emailValue = formData.get("username") as string;
-		console.log(emailValue);
 		navigate('/gender');
 	}
 
@@ -20,19 +41,56 @@ export function Age() {
 		<div className={s.container}>
 			<div className={s.box}>
 				<div className={s.logo}>
-					<img src={logo} alt='calendar'/>
+					<img src={logo} alt="calendar" />
 				</div>
 				<div className={s.description}>
 					<p>Your date of birth</p>
 				</div>
 				<form onSubmit={handleSubmit}>
 					<div>
-						<input className={s.input} name='day' placeholder='Day'></input>
-						<input className={s.input} name='mounth' placeholder='Mounth'></input>
-						<input className={s.input} name='year' placeholder='Year'></input>
+						<select
+							className={s.select}
+							name="day"
+							value={selectedDay}
+							onChange={(e) => setSelectedDay(e.target.value)}
+						>
+							<option value="">Day</option>
+							{days.map((day) => (
+								<option key={day} value={day}>
+									{day}
+								</option>
+							))}
+						</select>
+						<select
+							className={s.select}
+							name="month"
+							value={selectedMonth}
+							onChange={(e) => setSelectedMonth(e.target.value)}
+						>
+							<option value="">Month</option>
+							{months.map((month, index) => (
+								<option key={index + 1} value={index + 1}>
+									{month}
+								</option>
+							))}
+						</select>
+						<select
+							className={s.select}
+							name="year"
+							value={selectedYear}
+							onChange={(e) => setSelectedYear(e.target.value)}
+						>
+							<option value="">Year</option>
+							{years.map((year) => (
+								<option key={year} value={year}>
+									{year}
+								</option>
+							))}
+						</select>
 					</div>
+					{!legalAge && <span className={s.error}>You must be of legal age to create an account</span>}
 					<div className={s.button}>
-						<ButtonNext />
+						<ButtonNext disabled={!legalAge || !selectedDay || !selectedMonth || !selectedYear}/>
 					</div>
 				</form>
 			</div>
